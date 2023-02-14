936ba81751dba5d5b11a6a73e5c5180588a1bc70
Joy Kumar Pal <joypal.23.jkp@gmail.com>
2023-02-15 01:34:10 +0600

    :sparkles: added rounded circular & icon to button component.

--- src/components/button/ArktikButton.vue
+++ src/components/button/ArktikButton.vue
@@ -1,18 +1,25 @@
 <script lang="ts" setup>
 import { computed, useAttrs } from "vue";
-
-interface IProps {
+import type { Nullable } from "@/types";
+interface PropsButton {
   type?: "primary" | "secondary" | "transparent";
   size?: "default" | "sm" | "lg" | "full-width";
-  label: string;
-  loading: boolean;
+  rounded?: boolean;
+  label?: string;
+  loading?: boolean;
+  circular?: boolean;
+  icon?: Nullable<string>;
 }
+
 const attrs = useAttrs();
-const props = withDefaults(defineProps<IProps>(), {
+const props = withDefaults(defineProps<PropsButton>(), {
   type: "primary",
   size: "default",
   label: "",
   loading: false,
+  rounded: false,
+  circular: false,
+  icon: null,
 });
 
 const defaultAriaLabel = computed(() => {
@@ -21,24 +28,40 @@ const defaultAriaLabel = computed(() => {
 const disabled = computed(() => {
   return props.loading || attrs.disabled;
 });
+const classCircular = computed(() => {
+  const style = "rounded-full flex justify-center items-center ";
+  return style.concat(
+    props.circular
+      ? props.size === "sm"
+        ? "w-[32px] h-[32px]"
+        : props.size === "lg"
+        ? "w-[64px] h-[64px]"
+        : "w-[48px] h-[48px]"
+      : ""
+  );
+});
 </script>
 <template>
   <button
     type="button"
     :aria-label="defaultAriaLabel"
     :disabled="disabled"
-    :class="{
-      'base-button': true,
-      'small-button': props.size === 'sm',
-      'large-button': props.size === 'lg',
-      'w-full py-4': props.size === 'full-width',
-      'secondary-button': props.type === 'secondary',
-      'transparent-button': props.type === 'transparent',
-    }"
+    :class="[
+      {
+        'base-button': true,
+        'small-button': props.size === 'sm',
+        'large-button': props.size === 'lg',
+        'w-full py-4': props.size === 'full-width',
+        'secondary-button': props.type === 'secondary',
+        'transparent-button': props.type === 'transparent',
+        'rounded-full': props.rounded,
+      },
+      classCircular,
+    ]"
   >
-    <slot>
-      <span v-if="loading">Loading...</span>
-      <span v-else>{{ label || "&nbsp;" }}</span>
+    <span v-if="loading">Loading...</span>
+    <slot v-else>
+      <span>{{ label || "&nbsp;" }}</span>
     </slot>
   </button>
 </template>
